d51cf6c29a29cf34ec3a7000bd75b791b8255f33
NeshaRK08 <nesharavichandran@gmail.com>
2026-09-08 11:48:06 +0530

    Update frontend application structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from './context/ThemeContext'
 import Layout from './components/layout/Layout'
 import HomePage from './pages/HomePage'
 import PredictPage from './pages/PredictPage'
@@ -12,22 +11,20 @@ import './App.css'
 
 function App() {
   return (
-    <ThemeProvider>
-      <PredictionProvider>
-        <Router>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/predict" element={<PredictPage />} />
-              <Route path="/explainability" element={<ExplainabilityPage />} />
-              <Route path="/optimization" element={<OptimizationPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-            </Routes>
-          </Layout>
-        </Router>
-      </PredictionProvider>
-    </ThemeProvider>
+    <PredictionProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/predict" element={<PredictPage />} />
+            <Route path="/explainability" element={<ExplainabilityPage />} />
+            <Route path="/optimization" element={<OptimizationPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </PredictionProvider>
   )
 }
 
